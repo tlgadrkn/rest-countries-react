6bceb564134793678aad995224ad030284c5711d
Tolga Durukan <tolgadurukan16@gmail.com>
2021-02-02 13:04:48 +0200

    update readme

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import Country from '../Country/Country.js';
 import { CountryContext } from '../../context/GlobalState';
 import styles from './Main.module.css';
+import { FixedSizeList as List } from 'react-window';
 
 const Main = () => {
   const { countries } = useContext(CountryContext);
@@ -11,21 +12,29 @@ const Main = () => {
       <main className={styles.main}>
         {countries.map((country) => {
           return (
-            <Country
-              alpha3Code={country.alpha3Code}
-              countryName={country.name}
-              countryFlag={country.flag}
-              countryPopulation={country.population}
-              countryRegion={country.region}
-              countryCapital={country.capital}
-              topLevelDomain={country.topLevelDomain}
-              nativeName={country.nativeName}
-              subregion={country.subregion}
-              borders={country.borders}
-              currencies={country.currencies}
-              languages={country.languages}
-              key={country.alpha3Code}
-            />
+            <List
+              className='List'
+              height={1000}
+              itemCount={1000}
+              itemSize={35}
+              width={900}
+            >
+              <Country
+                alpha3Code={country.alpha3Code}
+                countryName={country.name}
+                countryFlag={country.flag}
+                countryPopulation={country.population}
+                countryRegion={country.region}
+                countryCapital={country.capital}
+                topLevelDomain={country.topLevelDomain}
+                nativeName={country.nativeName}
+                subregion={country.subregion}
+                borders={country.borders}
+                currencies={country.currencies}
+                languages={country.languages}
+                key={country.alpha3Code}
+              />
+            </List>
           );
         })}
       </main>
